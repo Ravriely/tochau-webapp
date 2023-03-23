b70f3a2db8b454e7212d8605e16e2b62fc41b1e4
Ravriely <77391472+Ravriely@users.noreply.github.com>
2023-03-23 14:18:11 +0100

    feat: index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,127 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main className={styles.main}>
+				<div className={styles.heading}>
+					<h1>Tochau</h1>
+					<h2>The next generation of organization apps</h2>
+				</div>
 
+				<div className={styles.center}>
+					<a
+						href="/"
+						className={styles.button}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<h2 className={inter.className}>
+							Start Now <span>-&gt;</span>
+						</h2>
+					</a>
+
+					<a
+						href="/"
+						className={styles.button}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<h2 className={inter.className}>
+							Login <span>-&gt;</span>
+						</h2>
+					</a>
+				</div>
+
+				<div className={styles.grid}>
+					<a
+						href="/"
+						className={styles.card}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<h2 className={inter.className}>
+							Tasks Management
+							<Image
+								src="https://img.icons8.com/fluency/48/null/bookmark-ribbon.png"
+								alt="Bookmark Icon"
+								className={styles.cardIcon}
+								width={32}
+								height={32}
+								priority
+							/>
+						</h2>
+						<p className={inter.className}>
+							Organize yourself or your team with our task management dashboard.
+						</p>
+					</a>
+
+
+					<a
+						href="/"
+						className={styles.card}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<h2 className={inter.className}>
+							InApp Chatting
+							<Image
+								src="https://img.icons8.com/fluency/48/null/speech-bubble.png"
+								alt="Speach Bubble Icon"
+								className={styles.cardIcon}
+								width={32}
+								height={32}
+								priority
+							/>
+						</h2>
+						<p className={inter.className}>
+							Share your troughts and ideas directly in our app for each task.
+						</p>
+					</a>
+
+
+					<a
+						href="/"
+						className={styles.card}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<h2 className={inter.className}>
+							Events Planner
+							<Image
+								src="https://img.icons8.com/fluency/48/null/calendar.png"
+								alt="Bookmark Icon"
+								className={styles.cardIcon}
+								width={32}
+								height={32}
+								priority
+							/>
+						</h2>
+						<p className={inter.className}>
+							Plan your outcoming patches, meetings and more in our modular planner.
+						</p>
+					</a>
+
+
+					<a
+						href="/"
+						className={styles.card}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<h2 className={inter.className}>
+							Tasks Management
+							<Image
+								src="https://img.icons8.com/fluency/48/null/lock-2.png"
+								alt="Free and Secure"
+								className={styles.cardIcon}
+								width={32}
+								height={32}
+								priority
+							/>
+						</h2>
+						<p className={inter.className}>
+							We protect your data with account login from Google or Microsoft.
+						</p>
+					</a>
+				</div>
 			</main>
 		</>
 	)
